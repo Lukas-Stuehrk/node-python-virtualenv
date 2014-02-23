8a43d1858d4c3988bb99a696a4a8bef97d112446
Lukas Stührk <Lukas@Stuehrk.net>
2014-02-23 21:27:03 +0100

    Made the tests version independent.

--- tests/test.sh
+++ tests/test.sh
@@ -2,9 +2,10 @@
 
 # Get the python version, but only Major and Minor
 VERSION=$(python -c 'import sys; print "%d.%d" % sys.version_info[:2]')
-echo ${VERSION}
+# Get the package's version.
+PACKAGE_VERSION=$(python -c 'from json import load;print(load(open("package.json"))["version"])')
 
 npm pack
-npm install python-virtualenv-0.0.1.tgz
+npm install python-virtualenv-${PACKAGE_VERSION}.tgz
 tests/test.js
 ls "node_modules/python-virtualenv/env/lib/python"${VERSION}"/site-packages/"

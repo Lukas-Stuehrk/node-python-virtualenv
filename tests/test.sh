9f57a8e4a03ae4058ede46a4bd5b9467e884d7f6
Lukas Stührk <l.stuehrk@gini.net>
2014-02-24 11:40:34 +0100

    Better tests.

--- tests/test.sh
+++ tests/test.sh
@@ -5,7 +5,17 @@ VERSION=$(python -c 'import sys; print "%d.%d" % sys.version_info[:2]')
 # Get the package's version.
 PACKAGE_VERSION=$(python -c 'from json import load;print(load(open("package.json"))["version"])')
 
+# Delete the directory with the virtualenv if it exists.
+if [ -d "node_modules/python-virtualenv/env/" ]; then
+  rm -rf node_modules/python-virtualenv/env
+fi
+
 npm pack
 npm install python-virtualenv-${PACKAGE_VERSION}.tgz
 tests/test.js
+
 ls "node_modules/python-virtualenv/env/lib/python"${VERSION}"/site-packages/"
+if [ ! -d "node_modules/python-virtualenv/env/lib/python"${VERSION}"/site-packages/zope/" ]; then
+  echo "zope.interface not installed!"
+  exit 1
+fi
